58b79771fee5a9a2d2fda56b46cd62ec1ee2af7d
Mobin <Mobinmojiri1234@gmail.com>
2025-06-28 17:26:54 +0330

    paginations

--- config/settings.py
+++ config/settings.py
@@ -124,9 +124,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+from rest_framework.pagination import PageNumberPagination
+
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',  # برای نمایش رابط وب
-    ]
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
 }
